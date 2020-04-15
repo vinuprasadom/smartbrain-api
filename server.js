c71d824bf7ede858f876251fdc1e310762cd561a
vinuprasadom <49249104+vinuprasadom@users.noreply.github.com>
2020-04-15 17:56:48 +0530

    for DEPTH_ZERO_SELF_SIGNED_CERT error test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
                client: 'pg',
                connection: {
                   connectionString: process.env.DATABASE_URL,
-                  ssl: true
+                  ssl: false
                   // host : '127.0.0.1',
                   // user : 'smartbrain',
                   // password : 'Smart',
